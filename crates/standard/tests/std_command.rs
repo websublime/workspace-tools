3beae5c59bafdf7ede0f7b53bd2202aa2e434ea1
Miguel Ramos <miguel.ramos@websublime.dev>
2024-10-29 01:27:33 +0000

    chore: execute command tests

--- crates/standard/tests/std_command.rs
+++ crates/standard/tests/std_command.rs
@@ -1,14 +1,23 @@
 #[cfg(test)]
 mod commands_tests {
+    use ws_std::command::execute;
+
     #[test]
     fn test_git_command() -> Result<(), Box<dyn std::error::Error>> {
-        use ws_std::command::execute;
-
         let result = execute("git", ".", ["--version"], |_message, output| {
             Ok(output.status.code().unwrap())
         })?;
 
         assert!(result == 0);
+        Ok(())
+    }
+
+    #[test]
+    fn test_git_version() -> Result<(), Box<dyn std::error::Error>> {
+        let result =
+            execute("git", ".", ["--version"], |message, _output| Ok(message.to_string()))?;
+
+        assert!(result.contains("git version"));
         Ok(())
     }
 }
